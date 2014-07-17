10d377bb5618ae43515adafe5921b8c53f0e929c
Shraddha Pai <Shraddha.Pai@camh.ca>
2014-07-17 13:48:09 -0400

    Adding html version of sphinx doc

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Shiny Genome Browse-R&#8217;s documentation!"],objects:{},titleterms:{welcom:0,tabl:0,shini:0,indic:0,genom:0,document:0,brows:0}})
